9d819928e248859e9c5bc68b15a26c46ce90f3c3
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-27 07:43:43 +0200

    Add check_currency @ main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import requests
-
 from bs4 import BeautifulSoup
+import time
 
 USD_UAH = 'https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%B0%D1%80%D0%B0+%D0%B4%D0%BE+%D0%B3%D1%80%D0%B8%D0%B2%D0%BD%D1%96&oq=%D0%BA%D1%83%D1%80&aqs=chrome.0.69i59j69i57j35i39j69i59j0j69i61l3.1353j0j7&sourceid=chrome&ie=UTF-8'
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
 
-full_page = requests.get(USD_UAH, headers=headers)
-# print(full_page.content)
 
-soup = BeautifulSoup(full_page.content, 'html.parser')
-# print(soup)
+def check_currency():
+    full_page = requests.get(USD_UAH, headers=headers)
+    # print(full_page.content)
 
-convert = soup.findAll('span', {'class': 'DFlfde', 'class': 'SwHCTb', 'data-precision': 2})
-print(convert[0].text)
+    soup = BeautifulSoup(full_page.content, 'html.parser')
+    # print(soup)
+
+    convert = soup.findAll('span', {'class': 'DFlfde', 'class': 'SwHCTb', 'data-precision': 2})
+    print("At the moment 1 USD = ", convert[0].text)
+    time.sleep(3)
+    check_currency()
+
+
+check_currency()
